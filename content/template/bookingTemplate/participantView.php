b7da0af58d5c5f087e32885f72ef4a6661493ea8
Måns Falk <mafa@multinet.se>
2017-11-07 10:22:15 +0100

    Revert "Moved the code for printing sub events to make it reusable since the same code is used more than once"
    This reverts commit 4913e5b

--- content/template/bookingTemplate/participantView.php
+++ content/template/bookingTemplate/participantView.php
@@ -124,7 +124,29 @@
                 </label>
 			<?php } ?>
 			<?php
-				include( "shared/subEvents.php" );
+				if ( count( $subEvents ) > 0 ) {
+					echo "<h4>" . edu__( "Sub events" ) . "</h4>\n";
+					foreach ( $subEvents as $subEvent ) {
+						if ( count( $sePrice[ $subEvent->OccasionID ] ) > 0 ) {
+							$s = current( $sePrice[ $subEvent->OccasionID ] )->Price;
+						} else {
+							$s = 0;
+						}
+						// PriceNameVat
+						echo "<label>" .
+						     "<input class=\"subEventCheckBox\" data-price=\"" . $s . "\" onchange=\"eduBookingView.UpdatePrice();\" " .
+						     "name=\"contactSubEvent_" . $subEvent->EventID . "\" " .
+						     "type=\"checkbox\"" .
+						     ( $subEvent->SelectedByDefault == true || $subEvent->MandatoryParticipation == true ? " checked=\"checked\"" : "" ) .
+						     ( $subEvent->MandatoryParticipation == true ? " disabled=\"disabled\"" : "" ) .
+						     " value=\"" . $subEvent->EventID . "\"> " .
+						     $subEvent->Description .
+						     ( $hideSubEventDateInfo ? "" : " (" . date( "d/m H:i", strtotime( $subEvent->StartDate ) ) . " - " . date( "d/m H:i", strtotime( $subEvent->EndDate ) ) . ") " ) .
+						     ( $s > 0 ? " <i class=\"priceLabel\">" . convertToMoney( $s ) . "</i>" : "" ) .
+						     "</label>\n";
+					}
+					echo "<br />";
+				}
 			?>
         </div>
         <div class="participantItem template" style="display: none;">
@@ -227,7 +249,31 @@
                     </div>
                 </label>
 			<?php } ?>
-			<?php include( "shared/subEvents.php" ); ?>
+			<?php
+				if ( count( $subEvents ) > 0 ) {
+					echo "<h4>" . edu__( "Sub events" ) . "</h4>\n";
+					foreach ( $subEvents as $subEvent ) {
+						if ( count( $sePrice[ $subEvent->OccasionID ] ) > 0 ) {
+							$s = current( $sePrice[ $subEvent->OccasionID ] )->Price;
+						} else {
+							$s = 0;
+						}
+						// PriceNameVat
+						echo "<label>" .
+						     "<input class=\"subEventCheckBox\" data-price=\"" . $s . "\" onchange=\"eduBookingView.UpdatePrice();\" " .
+						     "name=\"participantSubEvent_" . $subEvent->EventID . "[]\" " .
+						     "type=\"checkbox\"" .
+						     ( $subEvent->SelectedByDefault == true || $subEvent->MandatoryParticipation == true ? " checked=\"checked\"" : "" ) .
+						     ( $subEvent->MandatoryParticipation == true ? " disabled=\"disabled\"" : "" ) .
+						     " value=\"" . $subEvent->EventID . "\"> " .
+						     $subEvent->Description .
+						     ( $hideSubEventDateInfo ? "" : " (" . date( "d/m H:i", strtotime( $subEvent->StartDate ) ) . " - " . date( "d/m H:i", strtotime( $subEvent->EndDate ) ) . ") " ) .
+						     ( $s > 0 ? " <i class=\"priceLabel\">" . convertToMoney( $s ) . "</i>" : "" ) .
+						     "</label>\n";
+					}
+					echo "<br />";
+				}
+			?>
         </div>
     </div>
     <div>

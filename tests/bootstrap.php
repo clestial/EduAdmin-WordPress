7d6bb88b870a00cf8026923e1d211eb1576d7501
Chris Gårdenberg <chga@multinet.se>
2018-02-22 13:38:38 +0100

    Argh, why did the tests break? :(

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?";
+	exit( 1 );
+}
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
 define( 'PLUGIN_NAME', 'eduadmin.php' );
 define( 'PLUGIN_FOLDER', basename( dirname( __DIR__ ) ) );
 define( 'PLUGIN_PATH', PLUGIN_FOLDER . '/' . PLUGIN_NAME );
